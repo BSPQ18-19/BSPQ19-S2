3ce44b98e451d6cd6ef13941ea1b2413ca989195
anderja <33100283+anderja@users.noreply.github.com>
2019-04-01 19:30:33 +0200

    Add files via upload

---
+++ proyecto/src/main/java/com/DTO/app/AdminDTO.java
@@ -0,0 +1,17 @@
+package com.DTO.app;
+
+import java.io.Serializable;
+
+public class AdminDTO extends UsuarioDTO implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	public AdminDTO(String email, String contrasenya, boolean admin) {
+		super(email, contrasenya, admin);
+		// TODO Auto-generated constructor stub
+		admin=true;
+	}
+
+	
+
+}
