a936a3e3352a7c7b97aa4594d9b0025304ab5ae0
anderja <33100283+anderja@users.noreply.github.com>
2019-04-01 19:27:10 +0200

    Add files via upload

---
+++ proyectoCliente/src/main/java/com/proyectoCliente/app/App.java
@@ -0,0 +1,13 @@
+package com.proyectoCliente.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
