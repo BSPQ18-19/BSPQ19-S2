a6d114bfac2c799b0c27de7cf0a7c2a2f724c3cf
Iñigo de Mingo <inigo.demingo@opendeusto.es>
2019-04-02 12:50:23 +0200

    prueba1

--- proyecto/src/main/java/com/DTO/app/AdminDTO.java
+++ proyecto/src/main/java/com/DTO/app/AdminDTO.java
@@ -5,13 +5,14 @@ import java.io.Serializable;
 public class AdminDTO extends UsuarioDTO implements Serializable{
 
 	private static final long serialVersionUID = 1L;
-	
+
+
 	public AdminDTO(String email, String contrasenya, boolean admin) {
 		super(email, contrasenya, admin);
 		// TODO Auto-generated constructor stub
 		admin=true;
 	}
 
-	
+
 
 }
