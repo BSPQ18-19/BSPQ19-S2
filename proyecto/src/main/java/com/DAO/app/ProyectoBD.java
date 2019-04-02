f2a7d031e9a60541c058810a703cc6e3c8566a4f
victormartin <victormartin@opendeusto.es>
2019-04-02 18:30:29 +0200

    Implementación de Query en DAO

--- proyecto/src/main/java/com/DAO/app/ProyectoBD.java
+++ proyecto/src/main/java/com/DAO/app/ProyectoBD.java
@@ -15,5 +15,110 @@ public class ProyectoBD {
 				.getPersistenceManagerFactory("datanucleus.properties");
 	}
 	
+	@Override
+	public void registrar(Usuario usuario) {
+		// TODO Auto-generated method stub
+		PersistenceManager persistentManager = persistentManagerFactory.getPersistenceManager();
+		Transaction transaction = persistentManager.currentTransaction();
 
-}
+		try {
+			transaction.begin();
+			System.out.println("   * Guardando objeto: " + usuario.getNombre());
+			persistentManager.makePersistent(usuario);
+			// Fin de la transaccion
+			transaction.commit();
+			System.out.println(" -Datos guardados");
+
+		} catch (Exception ex) {
+			System.err.println("* Excepcion insertando datos en la BD: " + ex.getMessage());
+		} finally {
+			if (transaction.isActive()) {
+				transaction.rollback();
+			}
+
+			if (persistentManager != null && !persistentManager.isClosed()) {
+				persistentManager.close();
+			}
+		}
+	}
+	
+
+	@Override
+	public Usuario getUsuario(String email) {
+		// TODO Auto-generated method stub
+		PersistenceManager pm = persistentManagerFactory.getPersistenceManager();
+		pm.getFetchPlan().setMaxFetchDepth(3);
+		
+		Transaction tx = pm.currentTransaction();
+		Usuario usuario = null;
+	    
+		try {
+			System.out.println ("   * Sacando usuario con email: " + mail);
+			
+	    	tx.begin();
+	    	
+	    	@SuppressWarnings("unchecked")
+			Query<Usuario> query = pm.newQuery("SELECT FROM " +Usuario.class.getName()+" WHERE email == '"+email+"'");
+
+	    	query.setUnique(true);
+	    	usuario = (Usuario)query.execute();	 
+			
+		
+	    	query.close();	    
+ 	    	tx.commit();
+   	    
+	     } catch (Exception ex) {
+		   	System.out.println("   $ Error recuperando usuario: " + ex.getMessage());
+	     } finally {
+		   	if (tx != null && tx.isActive()) {
+		   		tx.rollback();
+		 }
+				
+	   		pm.close();
+	     }
+
+	    return usuario;
+	}
+	
+	public void updateUsuario(Usuario usuario) {
+		PersistenceManager pm = persistentManagerFactory.getPersistenceManager();
+		Transaction tx = pm.currentTransaction();
+		try {
+			tx.begin();
+			pm.makePersistent(usuario);
+			tx.commit();
+		}catch(Exception ex) {
+			System.out.println("  $ Error actualizando usuario: "+ ex.getMessage());
+		}finally {
+			if(tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+			pm.close();
+		}
+	}
+
+	public void borrarUsuario() {
+	System.out.println(" Borrando usuario ");
+	PersistenceManager pm = persistentManagerFactory.getPersistenceManager();
+	Transaction tx = pm.currentTransaction();
+	Usuario usuario = null;
+	try {
+		tx.begin();
+		pm.flush();
+		Query<Usuario> query1= pm.newQuery("DELETE FROM "+Usuario.class.getName()+" WHERE email == '"+email+"'");
+		query.setUnique(true);
+    	usuario = (Usuario)query.execute();	 
+    	query.close();	    
+	    	tx.commit();
+	    
+     } catch (Exception ex) {
+	   	System.out.println("   $ Error eliminando usuario: " + ex.getMessage());
+     } finally {
+	   	if (tx != null && tx.isActive()) {
+	   		tx.rollback();
+	 }
+			
+   		pm.close();
+     }
+	}
+	
