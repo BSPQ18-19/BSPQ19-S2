3f9cc117c4c21940330ad3d728004b891803e5ba
victormartin <victormartin@opendeusto.es>
2019-04-26 12:10:40 +0200

    Tests

---
+++ ProyectoProcesos/src/Test/ClientTest.java
@@ -0,0 +1,34 @@
+package es.deusto.testing.junit;
+//junit
+import static org.junit.Assert.assertEquals;
+
+import java.rmi.RemoteException;
+
+import org.junit.Test;
+
+import es.deusto.server.IServer;
+import es.deusto.server.jdo.Usuario;
+
+public class ClientTest {
+	static IServer server;
+	
+	@Test
+	public void testRegistrarUsuario() {
+		
+		Usuario user = null;
+		try {
+			user = server.registrarUsuario("prueba", "prueba", false);
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			user = server.comprobarUsuario("prueba", "prueba");
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertEquals(user, user);
+	}
+
+}
