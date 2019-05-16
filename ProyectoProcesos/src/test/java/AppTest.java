e0bd5ccc03d2d7c0f1ab9c46835ced935fd1c135
Iñigo de Mingo <inigo.demingo@opendeusto.es>
2019-05-16 19:18:40 +0200

    Test añadidos a >ander<

---
+++ ProyectoProcesos/src/test/java/AppTest.java
@@ -0,0 +1,16 @@
+package es.deusto.testing;
+
+import static org.junit.Assert.assertTrue;
+
+public class AppTest {
+	
+	@Test
+	public void shouldAnswerWithTrue()
+	{
+		
+		assertTrue(true);
+		
+	}
+	
+
+}
